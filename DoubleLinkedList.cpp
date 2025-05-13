5419c5c28f28182b7877daf6b75c2124905328f5
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-05-13 10:06:57 +0700

    membuat pointer next da pref

---
+++ DoubleLinkedList.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+//membuat struktur untuk object double linkedlist
+struct mode
+{
+    //deklarasi noMhs dan nama untuk menampung data
+    int noMhs;
+    string name;
+#include <iostream>
+using namespace std;
+//membuat struktur untuk object double linkedlist
+struct mode
+{
+    //deklarasi noMhs dan nama untuk menampung data
+    int noMhs;
+    string name;
+
+    //deklarasi pointer next da pref untuk penunjukan data sebelum dan sesudah 
+    Node *next;
+    Node *prev;
+};
+

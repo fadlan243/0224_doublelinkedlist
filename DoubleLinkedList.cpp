6ca4bad622add34df044a14e3315843636ab9ca4
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-05-13 10:44:43 +0700

    set nilai next node baru

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -57,5 +57,16 @@ void addNode()
         //set nilai current = start dan nilai previous = null
         Node *current = START; //step 1.a: start the first node
         Node *previous = NULL; //step 1.b: previous node is null innitialy
+
+        //loopping selama current != null dan noMhs dari current lebih kecil dari
+        while (current != NULL & current->noMhs < newNode->noMhs)
+        {                          //step 1.c: traverse the list to find the
+        previous = current;     //step 1.d: move the previous to the current
+        current = current->next; //step 1.e: move the current to the next node
+        }
+
+        //set nilai next node baru = current dan prev node baru = previous
+        newNode->next = current; //step 4: make the next field of the new node
+        newNode->prev = previous; //step 5: make the previous field of the new node
     }
 }
